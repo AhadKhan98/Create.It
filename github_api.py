fcaf146d8e204005dbdfc0edced159e980e0d03d
Mondale Felix <mondalefelix@gmail.com>
2020-11-24 18:28:15 -0800

    initializes repo with README

--- github_api.py
+++ github_api.py
@@ -1,17 +1,40 @@
 import requests
+import argparse
+import os
 
-# github_token = input("Enter token")
-# repo_name = input("Enter repo name")
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", "-n", type=str, dest="name", required=True)
+args = parser.parse_args()
+repo_name = args.name
+# is_private = args.is_private
+
 url = "https://api.github.com"
 
-payload = '{"name": "CreationHere"}'
+# if is_private:
+# payload = '{"name": "' + repo_name + '", "private": true }'
+# else:
+payload = '{"name": "' + repo_name + '", "private": false }'
+
 
 headers = {
-	"Authorization": "token " + "",
+	"Authorization": "token " + "bfda1de229fde1ef300a2b4519fa08926b6320fb",
 	"Accept": "application/vnd.github.v3+json"
 }
 
-r = requests.post(url+"/user/repos", data=payload, headers=headers)
+try:
 
+	r = requests.post(url+"/user/repos", data=payload, headers=headers)
+	r.raise_for_status()
+
+except requests.exceptions.RequestException as err:
+	raise SystemExit(err)
+
+try:
+	os.system("git init")
+	os.system("git remote add origin https://github.com/MondaleFelix/" + repo_name + ".git")
+	os.system("echo '# " + repo_name + "' >> README.md")
+	os.system("git add . && git commit -m 'Initial Commit' && git push origin master")
+except FileExistsError as err:
+	raise SystemExit(err)
 
 
